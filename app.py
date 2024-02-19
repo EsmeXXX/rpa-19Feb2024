b86d32c6405cdd937a4f46f16958f992314880b7
EsmeXXX <160452079+EsmeXXX@users.noreply.github.com>
2024-02-19 12:18:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"]
            def index()
+return(render_template("index.html")
 
-          return(render_template(
+       if __
            
